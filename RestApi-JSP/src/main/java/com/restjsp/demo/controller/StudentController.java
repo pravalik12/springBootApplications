9d1536b2f24e7096c2de8c2e7c67744d5b9ce776
saipravalikreddy <saipravalik.reddy@talentica.com>
2021-05-28 10:38:20 +0530

    updated h2-rest project

--- RestApi-JSP/src/main/java/com/restjsp/demo/controller/StudentController.java
+++ RestApi-JSP/src/main/java/com/restjsp/demo/controller/StudentController.java
@@ -2,26 +2,29 @@ package com.restjsp.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.restjsp.demo.model.Student;
 import com.restjsp.demo.repository.StudentRepository;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StudentController {
 	
 	@Autowired
 	StudentRepository repo;
 	
-	@RequestMapping("/")
-	public String home() {
-		return "home.jsp";
-	}
+//	@RequestMapping("/")
+//	public String home() {
+//		return "home.jsp";
+//	}
 
-	@RequestMapping("/addStudent")
-	public String addStudent(Student student) {
+	@PostMapping("/addStudent")
+	public String addStudent(@RequestBody Student student) {
 		System.out.println(student.toString());
 		repo.save(student);
-		return "home.jsp";
+		return "Success";
 	}
 }
--- RestApi-JSP/src/main/java/com/restjsp/demo/model/Student.java
+++ RestApi-JSP/src/main/java/com/restjsp/demo/model/Student.java
@@ -2,8 +2,10 @@ package com.restjsp.demo.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "student")
 public class Student {
 	
 	@Id
